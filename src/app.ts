3cd41fed52895fd5bd3d6078482dfb414d76b476
martin-durkin <s00236994@atu.ie>
2026-09-17 16:35:06 +0100

    finished worksheet 1

--- src/app.ts
+++ src/app.ts
@@ -8,51 +8,53 @@ const PORT = process.env.PORT || 4000;
 
 const app: Application = express(); 
 
+
+app.use((req, _res, next) => {   
+
+ console.log(`${req.method} ${req.originalUrl}`); 
+
+ next(); 
+
+}); 
+
 app.get("/ping", async (_req : Request, res: Response) => { 
      res.json({ 
 
-    message: "hello from Martin "
+  message: "hello from Martinfdsafdfdsafdsa "
 
-    }); 
+  }); 
 
 }); 
 
 app.get('/bananas', async (_req : Request, res: Response) => { 
 
-    res.json({ 
+    res.json({ 
 
-    message: "this is bananas", 
+    message: "this is bananas gfgfds", 
 
-    }); 
+    }); 
 
 }); 
 
+
+
 app.get('/pineapples', async (_req : Request, res: Response) => { 
 
-    res.json({ 
+    res.json({ 
 
-    message: "this is pineapples", 
+    message: "this is pineapples", 
 
-    }); 
+    }); 
 
 }); 
 
 app.get('/fun', async (_req : Request, res: Response) => { 
 
-    res.json({ 
+    res.json({ 
 
-    message: "this is fun", 
+    message: "this is fun", 
 
-    }); 
-
-}); 
-
-
-app.use((req, _res, next) => {   
-
-    console.log(`${req.method} ${req.originalUrl}`); 
-
-    next(); 
+    }); 
 
 }); 
 
@@ -62,8 +64,8 @@ app.use((req, _res, next) => {  
 
 app.listen(PORT, () => { 
 
-    console.log("Server is running on port", PORT); 
+    console.log("Server is running on port", PORT); 
 
-    }); 
+    }); 
 
  
